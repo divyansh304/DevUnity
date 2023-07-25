3101a84f0134d1f94fafa1bcfca863e99a8bfa53
divyansh304 <divyanshtyagi304@gmail.com>
2023-07-26 00:11:07 +0530

    Initial commit

---
+++ Devconnector-main/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://Vaibhav123:vaibhav123@devconnector-shard-00-00.5l9hd.mongodb.net:27017,devconnector-shard-00-01.5l9hd.mongodb.net:27017,devconnector-shard-00-02.5l9hd.mongodb.net:27017/Devconnector?ssl=true&replicaSet=atlas-jy4gee-shard-0&authSource=admin&retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+};
